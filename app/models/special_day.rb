95a7892d1bc3ac44ebc62a41a1421851b600b82f
Ben Greig <aquillo@bigpond.net.au>
2022-11-28 14:02:55 +1100

    model work

--- app/models/special_day.rb
+++ app/models/special_day.rb
@@ -1,2 +1,4 @@
 class SpecialDay < ApplicationRecord
+  has_many :users, though: :couple
+  validates :title, date:, presence: true
 end
--- db/migrate/20221128015524_create_special_days.rb
+++ db/migrate/20221128015524_create_special_days.rb
@@ -1,7 +1,9 @@
 class CreateSpecialDays < ActiveRecord::Migration[7.0]
   def change
     create_table :special_days do |t|
-
+      t.references :couple, null: false, foreign_key: true
+      t.date :date
+      t.string :title
       t.timestamps
     end
   end
