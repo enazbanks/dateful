7d13b911781dea9f46a529cf8634b2b2bcdc0192
Ben Greig <aquillo@bigpond.net.au>
2022-11-29 12:31:38 +1100

    new bookings

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import FlatpickrController from "./flatpickr_controller"
+application.register("flatpickr", FlatpickrController)
