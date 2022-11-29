e1522b9af20f0280db88dce1ae18ee8b863ac63d
Ben Greig <aquillo@bigpond.net.au>
2022-11-29 11:00:43 +1100

    start of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+couple = Couple.new
+
+frank = User.create(
+  first_name: 'Frank',
+  last_name: 'liaison',
+  email: 'frank@lewagon.com',
+  password: 'franktest'
+)
+frank.couple = couple
+
+bertha = User.create(
+  first_name: 'Bertha',
+  last_name: 'liaison',
+  email: 'bertha@lewagon.com',
+  password: 'berthatest'
+)
+bertha.couple = couple
+
+couple = Couple.new
+
+date = Date.new(
+    title: 'zoomies playdate',
+    description: 'Playtime for Dogs',
+    address: "park near you",
+    cost: 56.35,
+    time: "1-2 hours"
+)
+date.save
+booking = Booking.new(
+  when: Date.today,
+  instructions: "none",
+  suprise: false
+  status: 3
+)
+booking.couple = couple
+booking.date = date
+booking.save
+25.times do
+  y = rand(0..5)
+  x = rand(0..5)
+  y = x if x > y
+  rating = Rating.new(
+    stars: y
+    comment:
+  )
+  rating.booking = booking
+  rating.save
+end
