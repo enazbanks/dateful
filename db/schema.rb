f738ca6cf139f76f5a0bd56e0cf5bf9ebddf369b
Ben Greig <aquillo@bigpond.net.au>
2022-12-01 15:12:35 +1100

    added instructions and secret instructions fields to experiences

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_052501) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_040814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_052501) do
     t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "instructions"
+    t.text "secret_instructions"
   end
 
   create_table "favorites", force: :cascade do |t|
